1349b68450a1e5235b8827ae0876ae57494ba0b8
Francisco <franmendi.fm@gmail.com>
2023-11-18 22:57:16 -0300

    Refactor personal info and contact info components

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,82 +1,62 @@
-import React from 'react'
-import PersonalInfo from '../components/ui/PersonalInfo'
-import ContactInfo from '../components/ui/ContactInfo' 
-import CVUpload from '../components/ui/CVUpload'
-import Navbar from '../components/ui/Navbar'
-import { Link } from 'react-router-dom'
-import UniversityInfo from '../components/ui/UniversityInfo'
-import Skill from '../components/ui/Skill'
+import React from "react";
+import PersonalInfo from "../components/ui/PersonalInfo";
+import ContactInfo from "../components/ui/ContactInfo";
+import CVUpload from "../components/ui/CVUpload";
+import Navbar from "../components/ui/Navbar";
+import { Link } from "react-router-dom";
+import Student from "../layouts/Student";
+import UniversityInfo from "../components/ui/UniversityInfo";
+// import Skill from "../components/ui/Skill"; 
 
 const Settings = () => {
   return (
     <>
-    <Navbar
-          esconde= {' hidden' } 
-          buttons={[    
-
-            //campanita
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
-              />
-            </svg>,
-            //home
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
-              />
-            </svg>,
-           ]}
+      <Navbar
+          buttons={[1,2,3]}
         />
+        {/* Main content */}
+        <div className="bg-gray-700 flex justify-center">
 
-    {/* <h1 className="text-2xl font-semibold mb-4">Detalles del postulante</h1> */}
-        {/* deberiamos tratar de hacer en esta pagina varias solapitas(4) para los distintos tipos de datos del usuario , 
-        como en la pagina original */}
-    <div className="container mx-auto py-8">
-      <h1 className="text-2xl font-semibold mb-4">Datos Personales</h1>
-      <PersonalInfo />
+        <main className="w-3/4 ml-8">
+          <div className="bg-white p-6 shadow-lg rounded-lg">
+            {/* PersonalInfo and ContactInfo side by side */}
+            <div className="flex flex-wrap -mx-2">
+              <div className="w-full md:w-1/2 px-2 mb-4 md:mb-0">
+                <h1 className="text-xl font-semibold mb-2">Datos Personales</h1>
+                <hr className="mb-4 "/>
+                <PersonalInfo />
+              </div>
+              <div className="w-full md:w-1/2 px-2">
+                <h1 className="text-xl font-semibold mb-2">Informacion de contacto</h1>
+                <hr className="mb-4 "/>
 
-      {/*este componente se podria colocar al lado de PersonalInfo con flex*/}
-      <h1 className="text-2xl font-semibold mb-4">Informacion de contacto</h1>
-      <ContactInfo />
-      <CVUpload />
-      <h1 className="text-2xl font-semibold mb-4">Datos Universitarios</h1>
-      <UniversityInfo/>
-      <h1 className="text-2xl font-semibold mb-4">Conocimientos</h1>
-      <Skill/>
-      
-      <button className="px-4 py-2 bg-gray-700 text-white rounded">
-          Guardar
-      </button>
+                <ContactInfo />
+              </div>
+            </div>
 
-      <Link to={"/"}>
-      <button className="px-4 py-2 text-black rounded">
-          Volver Atras
-      </button>
-      </Link>
-      
-    </div>
+            <CVUpload />
 
+            {/* UniversityInfo and Skill commented out for now */}
+            {/* <UniversityInfo/> */}
+            {/* <Skill/> */}
+
+            <div className="flex justify-between mt-6">
+              <button className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">
+                Guardar
+              </button>
+
+              <Link to="/">
+                <button className="px-4 py-2 bg-gray-300 text-black rounded hover:bg-gray-400">
+                  Volver Atras
+                </button>
+              </Link>
+            </div>
+          </div>
+        </main>
+        </div>
+  
     </>
-  )
-}
+  );
+};
 
-export default Settings
+export default Settings;

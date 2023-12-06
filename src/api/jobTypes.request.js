72a4007f952f360405a9f11cd09c3cce7a0a211b
Francisco <franmendi.fm@gmail.com>
2023-12-06 20:19:49 -0300

    Update job type API endpoints

--- src/api/jobTypes.request.js
+++ src/api/jobTypes.request.js
@@ -1,17 +1,17 @@
 import axios from "./axios";
  
 
-export const getJobTypesRequest = async () => axios.get("/jobtype");
+export const getJobTypesRequest = async () => axios.get("/jobtypes");
 
 export const getJobTypeFilterRequest = async (params) =>  axios.get("/jobtype",{
         params:params});
  
-export const createJobTypeRequest = async (job) => axios.post("/jobtype", job);
+export const createJobTypeRequest = async (job) => axios.post("/jobtypes", job);
 
-export const updateJobTypeRequest = async (job) => axios.put(`/jobtype/${job.id}`, job);
+export const updateJobTypeRequest = async (job) => axios.put(`/jobtypes/${job.id}`, job);
 
-export const deleteJobTypeRequest = async (id) => axios.delete(`/jobtype/${id}`);
+export const deleteJobTypeRequest = async (id) => axios.delete(`/jobtypes/${id}`);
 
-export const getJobTypeRequest = async (id) => axios.get(`/jobtype/${id}`);
+export const getJobTypeRequest = async (id) => axios.get(`/jobtypes/${id}`);
 
  
